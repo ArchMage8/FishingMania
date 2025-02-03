fc9f534d480b065afae55ad35177af9a7aae3ea0
Darryl Orleans <darryl.orleans@binus.ac.id>
2025-02-03 18:00:53 +0700

    dialogue setup
    The choice buttons no work

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using Ink.Runtime;
+using UnityEngine.EventSystems;
 
 public class DialogueManager : MonoBehaviour
 {
@@ -11,8 +12,14 @@ public class DialogueManager : MonoBehaviour
     public GameObject dialoguePanel;
     public TextMeshProUGUI dialogueText;
 
+    public TextAsset inkJSON; //This is the ink file that stores the dialogue, its supposed to be passed from a trigger script to the manager script
+                              //But because its just testing, the script will be store the inkJSON directly
+    
     private Story currentStory;
     private bool dialogueRunning;
+
+    [SerializeField] private GameObject[] choices;
+    private TextMeshProUGUI[] choicesText;
 
     private void Awake()
     {
@@ -33,9 +40,17 @@ public class DialogueManager : MonoBehaviour
     {
         dialogueRunning = false;
         dialoguePanel.SetActive(false);
+
+        choicesText = new TextMeshProUGUI[choices.Length];
+        int index = 0;
+        foreach(GameObject choice in choices)
+        {
+            choicesText[index] = choice.GetComponentInChildren<TextMeshProUGUI>();
+            index++;
+        }
     }
     
-    public void EnterDialogueMode(TextAsset inkJSON)
+    public void EnterDialogueMode()
     {
         currentStory = new Story(inkJSON.text);
         dialogueRunning = true;
@@ -51,15 +66,57 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text = "";
     }
 
-    public void ContinueStory()
+    public  void ContinueStory()
     {
         if (currentStory.canContinue)
         {
             dialogueText.text = currentStory.Continue();
+            DisplayChoices();
         }
         else
         {
             ExitDialogueMode();
         }
+    }
+
+    private void DisplayChoices()
+    {
+        List<Choice> currentChoices = currentStory.currentChoices;
+
+        if(currentChoices.Count > choices.Length)
+        {
+            Debug.LogError("More choices were given than possible");
+        }
+
+        int index = 0;
+
+        foreach(Choice choice in currentChoices)
+        {
+            choices[index].gameObject.SetActive(true);
+            choicesText[index].text = choice.text;
+            index++;
+        }
+
+        for(int i = index; i< choices.Length; i++)
+        {
+            choices[i].gameObject.SetActive(false);
+        }
+
+        StartCoroutine(SelectFirstChoice());
+    } 
+
+    private IEnumerator SelectFirstChoice()
+    {
+        //Event System requires we clear it first, then wait
+        //for at least one frame before we set the current selected object
+        
+        EventSystem.current.SetSelectedGameObject(null);
+        yield return new WaitForEndOfFrame();
+        EventSystem.current.SetSelectedGameObject(choices[0].gameObject);
+    }
+
+    public void MakeChoice(int choiceIndex)
+    {
+        currentStory.ChooseChoiceIndex(choiceIndex);
     }
 }
---
+++ Assets/Scripts/Dialogue/TestDialogue.ink
@@ -0,0 +1,15 @@
+-> main
+
+== main ==
+Welcome to the restaurant...
+What would you like to order?
+    +[Fish Pasty]
+        ->chosen("Fish Pasty")
+    +[Fish Pie]
+        ->chosen("Fish Pie")
+
+== chosen(dish) ==
+Ok i will serve {dish} in 10 minutes
+
+-> END
+

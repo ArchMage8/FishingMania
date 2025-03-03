88c1b9d0923ab1e2234815c47898119710ebee62
ArchMage8 <darryl.orleans@binus.ac.id>
2025-03-03 18:51:53 +0700

    World Setup Complete
    gameobjects added, next is code implementation

---
+++ Assets/Scripts/Extras/DynamicSortingLayer.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class DynamicSortingLayer : MonoBehaviour
+{
+    public SpriteRenderer Reference;
+    public SpriteRenderer TargetSprite;
+    public int Difference;
+    private int originalLayer;
+
+    private void Start()
+    {
+        if (TargetSprite != null)
+        {
+            originalLayer = TargetSprite.sortingOrder;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            AdjustLayer();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            ResetLayer();
+        }
+    }
+
+    private void AdjustLayer()
+    {
+        if (Reference != null && TargetSprite != null)
+        {
+            TargetSprite.sortingOrder = Reference.sortingOrder + Difference;
+        }
+    }
+
+    private void ResetLayer()
+    {
+        if (TargetSprite != null)
+        {
+            TargetSprite.sortingOrder = originalLayer;
+        }
+    }
+}

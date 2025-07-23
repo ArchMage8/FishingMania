9bbab230921c989fbbb4ccfe37658aabf4134133
Darryl Orleans <darryl.orleans@binus.ac.id>
2025-07-23 15:55:30 +0700

    For hans

--- Assets/Scripts/Day Cycle/Sleep_Function.cs
+++ Assets/Scripts/Day Cycle/Sleep_Function.cs
@@ -1,46 +1,76 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Sleep_Function : MonoBehaviour
 {
-
     public GameObject TransitionFade;
+    public GameObject Prompt_Panel;
+
+    [Space(10)]
+
     public GameObject F_Indicator;
-    
+
     private bool playerInRange = false;
     private bool hasInteracted = false;
-    private Animator animator;
+
+    private Animator FadeAnimator;
+    private Animator PromptAnimator;
 
     private void Start()
     {
         TransitionFade.SetActive(false);
-        animator = TransitionFade.GetComponent<Animator>();
+        Prompt_Panel.SetActive(false); // Hide panel at start
+        FadeAnimator = TransitionFade.GetComponent<Animator>();
+        PromptAnimator = Prompt_Panel.GetComponent<Animator>();
     }
 
     void Update()
     {
-        if (playerInRange && !hasInteracted && Input.GetKeyDown(KeyCode.F) && InventoryManager.Instance.SomeUIEnabled == false)
+        if (playerInRange && !hasInteracted && Input.GetKeyDown(KeyCode.F) && !InventoryManager.Instance.SomeUIEnabled)
         {
-            StartCoroutine(ResetDay());
+            InventoryManager.Instance.SomeUIEnabled = true;
+
+            Prompt_Panel.SetActive(true); // Show confirmation panel
             hasInteracted = true;
             F_Indicator.SetActive(false);
         }
     }
 
+    public void ConfirmSleep()
+    {
+        StartCoroutine(ResetDay());
+    }
+
+    public void CloseSleepPanel()
+    {
+       StartCoroutine(ClosePanel());
+    }
+
+    private IEnumerator ClosePanel()
+    {
+        hasInteracted = true;
+
+        PromptAnimator.SetTrigger("Exit");
+        yield return new WaitForSeconds(1f);
+        Prompt_Panel.SetActive(false);
+
+        F_Indicator.SetActive(false);
+        InventoryManager.Instance.SomeUIEnabled = false;
+    }
+
     private IEnumerator ResetDay()
     {
-        InventoryManager.Instance.SomeUIEnabled = true;
         
+        Prompt_Panel.SetActive(false); // Hide confirmation panel
+
         TransitionFade.SetActive(true);
         yield return new WaitForSeconds(0.5f);
-        animator.SetTrigger("Exit");
+        FadeAnimator.SetTrigger("Exit");
 
         Daylight_Handler.Instance.CallNewDay();
 
         yield return new WaitForSeconds(0.7f);
         InventoryManager.Instance.SomeUIEnabled = false;
-
         TransitionFade.SetActive(false);
     }
 
@@ -58,6 +88,7 @@ public class Sleep_Function : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             F_Indicator.SetActive(false);
+     
             playerInRange = false;
             hasInteracted = false;
         }
